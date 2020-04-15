59d871c6b9d6ba6452115976a4e795e9f59c83c3
Bruno Antunes <bmvantunes@gmail.com>
2020-04-15 20:48:51 +0100

    after the index and people component

---
+++ src/pages/People.tsx
@@ -0,0 +1,25 @@
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+
+export interface PeopleProps {
+  names: string[];
+}
+
+export default function People({ names }: PeopleProps) {
+  return (
+    <div>
+        <Link href="/">
+            <a>Index</a>
+        </Link>
+      {names.map((name) => {
+        return <h2 key={name}>{name}</h2>;
+      })}
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<PeopleProps> = async (
+  ctx
+) => {
+  return { props: { names: ['john', 'michael', 'bruno'] } };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,32 @@
-export default function Index() {
-  return <h1>Hello World</h1>;
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+import { Microphone } from '../../model/Microphone';
+import { openDB } from '../openDB';
+
+export interface IndexProps {
+  microphones: Microphone[];
 }
+
+export default function Index({ microphones }: IndexProps) {
+  return (
+    <div>
+      <Link href="/people">
+        <a>People</a>
+      </Link>
+      <pre>{JSON.stringify(microphones, null, 4)}</pre>;
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<IndexProps> = async (
+  ctx
+) => {
+  const db = await openDB();
+  const microphones = await db.all<Microphone[]>('select * from microphone');
+
+  await new Promise(acc => {
+    setTimeout(acc, 3000);
+  });
+
+  return { props: { microphones } };
+};
